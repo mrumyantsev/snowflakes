e8fec5c86c9a38e6ceb89b1e2937571615c7f0b5
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-04-01 13:31:09 +0300

    refactor: remake engine

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,76 +1,69 @@
-import pygame, simple_draw, random
+from random import random, randint
+import pygame, simple_draw as D
+import entities.snowflake as S
 
 
-class Snowflake:
-    counter = -1
+class EventMouseMove:
+    __moves_count = 0
 
-    def __init__(self, x, y, length, color, factor_a, factor_b, factor_c, speed, wind):
-        self.x = x
-        self.y = y
-        self.past_x = self.past_y = None
-        self.length = length
-        self.color = color
-        self.factor_a = factor_a
-        self.factor_b = factor_b
-        self.factor_c = factor_c
-        self.speed = speed
-        self.wind = wind
-        self.id = Snowflake.counter
-        Snowflake.counter += 1
-
-    def draw(self):
-        self.past_x, self.past_y = self.x, self.y
-        center = simple_draw.Point(self.past_x, self.past_y)
-        simple_draw.snowflake(center=center, length=self.length, color=simple_draw.background_color, factor_a=self.factor_a,
-                     factor_b=self.factor_b, factor_c=self.factor_c)
-        self.x *= self.wind
-        self.y -= self.speed
-        center = simple_draw.Point(self.x, self.y)
-        simple_draw.snowflake(center=center, length=self.length, color=self.color, factor_a=self.factor_a,
-                     factor_b=self.factor_b, factor_c=self.factor_c)
+    @staticmethod
+    def check_moves_reached_number(number) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                EventMouseMove.__moves_count += 1
+                break
+        
+        if EventMouseMove.__moves_count >= number:
+            return True
+        
+        return False
 
 
-class SnowflakesEngine:
+class Engine:
     def __init__(self, resolution=(640, 480), snowflakes_number=10) -> None:
-        simple_draw.resolution = resolution
-        simple_draw.background_color = (0, 0, 0)
+        D.resolution = resolution
+        D.background_color = D.COLOR_BLACK
 
         pygame.init()
         pygame.mouse.set_visible(False)
 
-        self.snowflakes_number = snowflakes_number
-        self.snowflakes = []
+        self.__snowflakes_number = snowflakes_number
+        self.__snowflakes = [
+            self.make_snowflake() for _ in range(self.__snowflakes_number)
+        ]
 
-        for _ in range(self.snowflakes_number):
-            self.make_snowflake()
+        self.__trail_color = D.COLOR_BLACK
     
-    def run(self):
-        mouse_moves = 0
+    def run(self) -> None:
         while True:
-            i = 0
-            while i < self.snowflakes_number:
-                self.snowflakes[i].draw()
-                if self.snowflakes[i].y < -170:
-                    self.snowflakes.pop(i)
-                    self.make_snowflake()
-                i += 1
-            simple_draw.sleep(0.005)
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEMOTION:
-                    mouse_moves += 1
-                    if mouse_moves > 3:
-                        pygame.quit()
-                        return
+            for i in range(self.__snowflakes_number):
+                self.__snowflakes[i].draw()
+                self.__snowflakes[i].move()
+                self.__snowflakes[i].draw_trail()
+                if self.__snowflakes[i].y < -170:
+                    self.__snowflakes[i] = self.make_snowflake()
+
+            if EventMouseMove.check_moves_reached_number(4):
+                pygame.quit()
+                break
+            
+            D.sleep(0.005)
     
-    def make_snowflake(self):
-        white_bright = random.randint(242, 254)
-        snowflake = Snowflake(x=random.randint(-120, simple_draw.resolution[0] + 20),
-                            y=simple_draw.resolution[1] + 70 + random.randint(-50, 720),
-                            length=random.randint(30, 95),
-                            color=(white_bright, white_bright, white_bright),
-                            factor_a=random.random(),
-                            factor_b=random.random(),
-                            factor_c=random.randint(1, 179),
-                            speed=random.randint(4, 12),
-                            wind=float("1.00" + str(random.randint(1, 3))))
-        self.snowflakes.append(snowflake)
+    def make_snowflake(self) -> S.Snowflake:
+        x = randint(-120, D.resolution[0] + 20)
+        y = D.resolution[1] + 70 + randint(-50, 720)
+        speed_x = 1.0
+        speed_y = randint(4, 12)
+        accel_x = float(str(randint(1, 3)))
+        size = randint(30, 95)
+        sub_color = randint(230, 250)
+        color = (sub_color, sub_color, sub_color)
+        trail_color = D.COLOR_BLACK
+        factor_a = random()
+        factor_b = random()
+        factor_c = 180 * random()
+
+        return S.Snowflake(x=x, y=y, speed_x=speed_x,
+                         speed_y=speed_y, accel_x=accel_x, size=size,
+                         color=color, trail_color=trail_color, factor_a=factor_a,
+                         factor_b=factor_b, factor_c=factor_c)
