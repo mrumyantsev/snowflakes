888990b1e5f92571914bedc0beae80723d0f7bf8
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-04-03 23:59:20 +0300

    style: make quotes in python style

--- lib/lib.py
+++ lib/lib.py
@@ -2,4 +2,4 @@ import os
 
 
 def get_app_dir(file_globvar=__file__) -> str:
-    return os.path.dirname(os.path.abspath(file_globvar)) + "\\"
+    return os.path.dirname(os.path.abspath(file_globvar)) + '\\'
