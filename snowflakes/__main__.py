e4b1f1919b838ae749831c43884afc69e2d62176
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-03-26 16:52:22 +0300

    refactor(__main__): remake config import

--- snowflakes/__main__.py
+++ snowflakes/__main__.py
@@ -1,10 +1,26 @@
-from lib import lib
+import os
+import sys
+
+
+# getting the name of the directory
+# where the this file is present.
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Getting the parent directory name
+# where the current directory is present.
+project_dir = os.path.dirname(current_dir)
+
+# adding the parent directory to
+# the sys.path.
+sys.path.append(project_dir)
+
+
 from config import config
 from engine import engine as engine_module
 
 
 def main() -> None:
-    cfg_path = lib.get_app_dir(__file__) + './configs/config.yml'
+    cfg_path = os.path.join(project_dir, './configs/config.yml')
     cfg = config.load_config(cfg_path)
 
     engine = engine_module.Engine(cfg)
